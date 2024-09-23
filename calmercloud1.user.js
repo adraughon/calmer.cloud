3e6922d04f7fa87a1600c9aefb15541b7e07cb86
adraughon <austin@radialflow.net>
2024-09-23 14:42:11 -0500

    Update calmercloud1.user.js

--- calmercloud1.user.js
+++ calmercloud1.user.js
@@ -7,8 +7,8 @@
 // @match        https://*/*
 // @icon         https://static.wixstatic.com/media/8cb6d1_8ad0e3bf9fad4d799f2ebffcebd4f136~mv2.png/v1/fill/w_686,h_686,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/medium_cloud.png
 // @grant        GM_xmlhttpRequest
-// @updateURL    https://raw.githubusercontent.com/adraughon/calmer-cloud/main/tampermonkey/calmercloud.user.js
-// @downloadURL  https://raw.githubusercontent.com/adraughon/calmer-cloud/main/tampermonkey/calmercloud.user.js
+// @updateURL    https://raw.githubusercontent.com/adraughon/calmer.cloud/main/calmercloud1.user.js
+// @downloadURL  https://raw.githubusercontent.com/adraughon/calmer.cloud/main/calmercloud1.user.js
 // ==/UserScript==
 
 (function() {
@@ -19,7 +19,7 @@
     function calm(items) {
         items.forEach(item => {
             try {
-                if (item.nodeType === Node.ELEMENT_NODE && !item.hasAttribute('show')) {
+                if (item.nodeType === Node.ELEMENT_NODE) {
                     if (item.tagName === 'IMG' || item.tagName === 'IMAGE') {
                         item.style.filter = `blur(${blurAmount})`;
                     }
@@ -76,29 +76,6 @@
         calm(document.querySelectorAll('*'));
     }, 100);
 
-    function checkModeration(texttocheck) {
-        GM_xmlhttpRequest({
-            method: "POST",
-            url: "https://api.openai.com/v1/chat/completions",
-            headers: {
-                "Content-Type": "application/json",
-                //"Authorization": "Bearer KEY" // Replace with your API key
-            },
-            data: JSON.stringify({
-                model: "gpt-3.5-turbo",
-                messages: [{ role: "user", content: texttocheck }],
-                max_tokens: 500
-            }),
-            onload: function(response) {
-                const result = JSON.parse(response.responseText);
-                const messageContent = result.choices[0].message.content;
-                console.log("Moderation Input:", texttocheck, "Moderation Result:", messageContent);
-            },
-            onerror: function(error) {
-                console.error("Request failed:", error);
-            }
-        });
-    }
 
     // End of file
 })();
